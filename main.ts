d471ec38cddf747dfdae510e058b950a0001ffac
Tiago Baker <tiago.baker@stu.ocsb.ca>
2026-01-08 13:10:18 -0500

    Button A test code

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Tiago Baker
+ * Created on: Jan 2026
+ * This program This program moves servo 0 and 80 degrees
 */
 
-basic.showString('Hello, World!')
+// variables 
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// button A pressed
+input.onButtonPressed(Button.A, function () {
+    // move servo to 80 and 0 degrees 6 times
+    robotbit.Servo(servoNumber1, 80)
+    basic.pause(500)
+    robotbit.Servo(servoNumber1, 0)
+    basic.pause(500)
+    robotbit.Servo(servoNumber1, 80)
+    basic.pause(500)
+    robotbit.Servo(servoNumber1, 0)
+    basic.pause(500)
+    robotbit.Servo(servoNumber1, 80)
+    basic.pause(500)
+    robotbit.Servo(servoNumber1, 0)
+    basic.pause(500)
+    basic.showString('bye')
+    basic.showIcon(IconNames.Happy)
+})
